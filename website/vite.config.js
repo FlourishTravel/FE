2049953a2697c1edcc2bff9abb6396b03cda83af
khanhtmse182131@fpt.edu.vn <khanhtmse182131@fpt.edu.vn>
2026-03-07 08:23:43 +0700

    [ADD]

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FlourishTravel/', // Đổi nếu tên repo GitHub khác
   plugins: [
     react(),
     tailwindcss(),
